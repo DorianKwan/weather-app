d6a2ccc26ab9b8b87c842a03e16f115274eb8551
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-03-04 06:32:30 -0700

    feat: create page wrapper utility component

---
+++ src/components/utility/PageWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const PageWrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  padding: 1.25rem;
+
+  @media screen and (min-width: 640px) {
+    padding: 3rem;
+  }
+`;
