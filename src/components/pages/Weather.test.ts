35a0fde7d5add0c3466783ba2a676e61da52680a
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-03-06 21:05:20 -0500

    feat: test weather page temp conversion

--- src/components/pages/Weather.test.ts
+++ src/components/pages/Weather.test.ts
@@ -1,9 +1,86 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { render, screen } from '../../utils';
 import { Weather } from './Weather';
 
-test('renders weather screen', () => {
-  render(React.createElement(Weather));
-  const weatherContainer = screen.getByLabelText(/weather/i);
+const mockAPI = jest.fn().mockResolvedValue({
+  base: '',
+  clouds: { all: 0 },
+  cod: 0,
+  coord: { lon: 0, lat: 0 },
+  dt: 0,
+  id: 0,
+  main: {
+    feels_like: 0,
+    humidity: 0,
+    pressure: 0,
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0,
+  },
+  name: 'Calgary',
+  timezone: 0,
+  visibility: 0,
+  weather: [{ description: '', icon: '', id: 0, main: '' }],
+  wind: {
+    deg: 0,
+    gust: 0,
+    speed: 0,
+  },
+});
+
+test('renders weather page', async () => {
+  render(React.createElement(Weather, { mockAPI }));
+
+  const weatherContainer = await screen.findByLabelText(/weather/i);
+
   expect(weatherContainer).toBeInTheDocument();
+});
+
+test('Temp change to Celsius', async () => {
+  render(React.createElement(Weather, { mockAPI }));
+
+  const kelvinButton = await screen.findByLabelText(/kelvin/i);
+
+  act(() => {
+    kelvinButton.click();
+  });
+
+  const celsiusButton = await screen.findByLabelText(/celsius/i);
+
+  act(() => {
+    celsiusButton.click();
+  });
+
+  const celsiusTemp = await screen.findByLabelText(/\d+ °C/);
+
+  expect(celsiusTemp).toBeInTheDocument();
+});
+
+test('Temp change to Fahrenheit', async () => {
+  render(React.createElement(Weather, { mockAPI }));
+
+  const fahrenheitButton = await screen.findByLabelText(/fahrenheit/i);
+
+  act(() => {
+    fahrenheitButton.click();
+  });
+
+  const fahrenheitTemp = await screen.findByLabelText(/\d+ °F/);
+
+  expect(fahrenheitTemp).toBeInTheDocument();
+});
+
+test('Temp change to Kelvin', async () => {
+  render(React.createElement(Weather, { mockAPI }));
+
+  const kelvinButton = await screen.findByLabelText(/kelvin/i);
+
+  act(() => {
+    kelvinButton.click();
+  });
+
+  const kelvinTemp = await screen.findByLabelText(/\d+K/);
+
+  expect(kelvinTemp).toBeInTheDocument();
 });
