fa77ca9fb9798ae09f2a7d250172a767e7372447
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2022-06-22 02:27:28 -0600

    refactor: mock axios for weather tests

--- src/components/pages/Weather.test.ts
+++ src/components/pages/Weather.test.ts
@@ -1,44 +1,50 @@
-import userEvent from '@testing-library/user-event';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
+import axios from 'axios';
 import { fireEvent, render, screen } from '../../utils';
 import { Weather } from './Weather';
 
-// must be lowercase for tests to work; targeted by label
-const mockCityName = 'calgary';
-const baseMockApiData = {
-  base: '',
-  clouds: { all: 0 },
-  cod: 0,
-  coord: { lon: 0, lat: 0 },
-  dt: 0,
-  id: 0,
-  main: {
-    feels_like: 0,
-    humidity: 0,
-    pressure: 0,
-    temp: 0,
-    temp_max: 0,
-    temp_min: 0,
-  },
-  name: mockCityName,
-  timezone: 0,
-  visibility: 0,
-  weather: [{ description: '', icon: '', id: 0, main: '' }],
-  wind: {
-    deg: 0,
-    gust: 0,
-    speed: 0,
-  },
-};
+jest.mock('axios');
 
-const mockAPI = jest.fn().mockResolvedValue(baseMockApiData);
-const mockAPINoTemp = jest
-  .fn()
-  .mockResolvedValue({ ...baseMockApiData, main: undefined });
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const mockCityName = 'calgary';
+
+mockedAxios.get.mockReturnValue(
+  Promise.resolve({
+    status: 200,
+    data: {
+      base: '',
+      clouds: { all: 0 },
+      cod: 0,
+      coord: { lon: 0, lat: 0 },
+      dt: 0,
+      id: 0,
+      main: {
+        feels_like: 0,
+        humidity: 0,
+        pressure: 0,
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0,
+      },
+      // must be lowercase for tests to work; targeted by label
+      name: 'calgary',
+      timezone: 0,
+      visibility: 0,
+      weather: [{ description: '', icon: '', id: 0, main: '' }],
+      wind: {
+        deg: 0,
+        gust: 0,
+        speed: 0,
+      },
+    },
+  }),
+);
 
 const setupWeather = async () => {
-  render(React.createElement(Weather, { mockAPI }));
+  render(React.createElement(Weather));
   const unitChangeButtons = {
     celsius: await screen.findByLabelText(/celsius/i),
     fahrenheit: await screen.findByLabelText(/fahrenheit/i),
@@ -54,62 +60,64 @@ const setupWeather = async () => {
   };
 };
 
-test('temp change to Celsius', async () => {
-  const { unitChangeButtons } = await setupWeather();
-  const { celsius: celsiusButton, kelvin: kelvinButton } = unitChangeButtons;
+describe('Weather.tsx', () => {
+  it('changes temp to Celsius when Celsius button is clicked', async () => {
+    const { unitChangeButtons } = await setupWeather();
+    const { celsius: celsiusButton, kelvin: kelvinButton } = unitChangeButtons;
 
-  act(() => {
-    kelvinButton.click();
-    celsiusButton.click();
+    act(() => {
+      kelvinButton.click();
+      celsiusButton.click();
+    });
+
+    const celsiusTemp = await screen.findByLabelText(/\d+ °C/);
+
+    expect(celsiusTemp).toBeInTheDocument();
   });
 
-  const celsiusTemp = await screen.findByLabelText(/\d+ °C/);
+  it('changes temp to Fahrenheit when Fahrenheit is clicked', async () => {
+    const { unitChangeButtons } = await setupWeather();
+    const { fahrenheit: fahrenheitButton } = unitChangeButtons;
 
-  expect(celsiusTemp).toBeInTheDocument();
-});
+    act(() => {
+      fahrenheitButton.click();
+    });
 
-test('temp change to Fahrenheit', async () => {
-  const { unitChangeButtons } = await setupWeather();
-  const { fahrenheit: fahrenheitButton } = unitChangeButtons;
+    const fahrenheitTemp = await screen.findByLabelText(/\d+ °F/);
 
-  act(() => {
-    fahrenheitButton.click();
+    expect(fahrenheitTemp).toBeInTheDocument();
   });
 
-  const fahrenheitTemp = await screen.findByLabelText(/\d+ °F/);
+  it('changes temp to Kelvin when Kelvin is clicked', async () => {
+    const { unitChangeButtons } = await setupWeather();
+    const { kelvin: kelvinButton } = unitChangeButtons;
 
-  expect(fahrenheitTemp).toBeInTheDocument();
-});
+    act(() => {
+      kelvinButton.click();
+    });
 
-test('temp change to Kelvin', async () => {
-  const { unitChangeButtons } = await setupWeather();
-  const { kelvin: kelvinButton } = unitChangeButtons;
+    const kelvinTemp = await screen.findByLabelText(/\d+K/);
 
-  act(() => {
-    kelvinButton.click();
+    expect(kelvinTemp).toBeInTheDocument();
   });
 
-  const kelvinTemp = await screen.findByLabelText(/\d+K/);
+  it('displays error when no weather data is returned', async () => {
+    const { cityInput } = await setupWeather();
 
-  expect(kelvinTemp).toBeInTheDocument();
-});
+    mockedAxios.get.mockResolvedValue(
+      Promise.resolve({
+        status: 404,
+        data: null,
+      }),
+    );
 
-test('new weather info is fetched when changing city', async () => {
-  const { cityInput } = await setupWeather();
+    userEvent.type(cityInput, 'Narnia');
+    fireEvent.keyPress(cityInput, { key: 'Enter', charCode: 13 });
 
-  userEvent.type(cityInput, 'toronto');
-  fireEvent.keyPress(cityInput, { key: 'Enter', charCode: 13 });
+    render(React.createElement(Weather));
 
-  const updatedCityName = await screen.findByLabelText('toronto');
+    const errorMessage = await screen.findByText(/Error Fetching Weather/i);
 
-  // only check if city name changes as temperature returned isn't consistent
-  expect(updatedCityName).toBeInTheDocument();
-});
-
-test('error shown when no weather data is returned', async () => {
-  render(React.createElement(Weather, { mockAPI: mockAPINoTemp }));
-
-  const errorMessage = await screen.findByText(/was not found/i);
-
-  expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).toBeInTheDocument();
+  });
 });
